b0ed761fa736df1db60c211051ad98a26cffdaf2
Nguyen The Khiem <126439080+thekhiem14@users.noreply.github.com>
2024-07-05 20:54:24 +0700

    Hien thi thong bao

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
+const flash = require('express-flash')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 require("dotenv").config()
 
 const database = require("./config/database")
@@ -23,11 +26,18 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.set("views","./views");
 app.set("view engine", "pug");
 
+// Flash
+app.use(cookieParser('ABCDEFGH'));
+app.use(session({ cookie: { maxAge: 60000 } }));
+app.use(flash());
+// End Flash
+
 app.locals.prefixAdmin = systemConfig.prefixAdmin
 // Khai báo biến prefixAdmin
 
 app.use(express.static("public"))
 
+// Route
 routeAdmin(app)
 route(app)
 
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -152,5 +152,23 @@ if (formChangeMulti) {
         }
     })
 }
-
 //End Form Change Multi
+
+// Show Alert
+const showAlert = document.querySelector("[show-alert]")
+if(showAlert){
+  const time=parseInt(showAlert.getAttribute("data-time"))
+  const closeAlert = showAlert.querySelector("[close-alert]")
+
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden")
+  }, time)  
+  // Sau 5 giay tu dong thong bao
+
+  closeAlert.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden")
+  })
+  // Chu dong dong thong bao
+  console.log(showAlert)
+}
+// End Show Alert
