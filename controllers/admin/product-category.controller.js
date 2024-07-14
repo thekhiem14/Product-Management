c92c0da4c07f75cb0afdd4769623b19e62bb952e
Nguyen The Khiem <126439080+thekhiem14@users.noreply.github.com>
2024-07-14 19:51:42 +0700

    Tạo danh mục cha

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -5,24 +5,47 @@ const systemConfig = require("../../config/system")
 
 // [GET] /admin/product-category
 module.exports.index = async (req, res) => {
-    let find = {
-      deleted: false,
-    };
-  
-    const records = await ProductCategory.find(find);
-  
-  
-    res.render("admin/pages/product-category/index", {
-      pageTitle: "Danh mục sản phẩm",
-      records: records 
-    });
+  let find = {
+    deleted: false,
   };
+  
+  const records = await ProductCategory.find(find);
+
+  res.render("admin/pages/product-category/index", {
+    pageTitle: "Danh mục sản phẩm",
+    records: records 
+  });
+};
   
 
 // [GET] /admin/product-category/create
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false,
+  }
+
+  function createTree(arr, parentId = ""){
+    const tree = [];
+    arr.forEach((item) => {
+      if (item.parent_id === parentId) {
+        const newItem = item;
+        const children = createTree(arr, item.id);
+        if (children.length > 0) {
+          newItem.children = children;
+        }
+        tree.push(newItem);
+      }
+    });
+    return tree;
+  }
+
+  const records = await ProductCategory.find(find);
+
+  const newRecords = createTree(records)
+  console.log(newRecords)
     res.render("admin/pages/product-category/create", {
-        pageTitle: "Danh muc san pham"
+        pageTitle: "Danh muc san pham",
+        records: newRecords
    })
 }
 
