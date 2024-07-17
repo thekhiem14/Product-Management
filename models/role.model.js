9b9de25386fe17f1c909abe13c7fb665eb1cb602
Nguyen The Khiem <126439080+thekhiem14@users.noreply.github.com>
2024-07-17 22:57:58 +0700

    Tạo collection role

---
+++ models/role.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+
+const roleSchema = new mongoose.Schema(
+    {
+        title: String,
+        description: String,
+        permissions: {
+            type: String,
+            default: []
+        },
+        deleted: {
+            type: Boolean,
+            default: false
+        },
+        deletedAt: Date
+    },
+    {
+        timestamps: true
+    },
+)
+
+const Role = mongoose.model('Role', roleSchema, "role")
+
+module.exports = Role
