f55c070dc776abc28e7ee56c8badf2e556567aad
Nguyen The Khiem <126439080+thekhiem14@users.noreply.github.com>
2024-07-23 10:50:26 +0700

    Middleware cho route private

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,7 @@
 const systemConfig = require("../../config/system")
 
+const authMiddleware = require("../../middlewares/admin/auth.middleware")
+
 const dashboardRoutes = require("./dashboard.route")
 const productRoutes = require("./product.route")
 const productCategoryRoutes = require("./product-category.route")
@@ -10,11 +12,11 @@ const authRoutes = require("./auth.route")
 module.exports = (app) => {
      const PATH_ADMIN = systemConfig.prefixAdmin
 
-     app.use(PATH_ADMIN + "/dashboard", dashboardRoutes)
-     app.use(PATH_ADMIN + "/product", productRoutes)
-     app.use(PATH_ADMIN + "/product-category", productCategoryRoutes)
-     app.use(PATH_ADMIN + "/role", roleRoutes)
-     app.use(PATH_ADMIN + "/account", accountRoutes)
+     app.use(PATH_ADMIN + "/dashboard", authMiddleware.requireAuth, dashboardRoutes)
+     app.use(PATH_ADMIN + "/product", authMiddleware.requireAuth, productRoutes)
+     app.use(PATH_ADMIN + "/product-category", authMiddleware.requireAuth, productCategoryRoutes)
+     app.use(PATH_ADMIN + "/role", authMiddleware.requireAuth, roleRoutes)
+     app.use(PATH_ADMIN + "/account", authMiddleware.requireAuth, accountRoutes)
      app.use(PATH_ADMIN + "/auth", authRoutes)
 
 }
