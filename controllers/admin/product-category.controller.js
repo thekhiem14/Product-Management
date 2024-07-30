1a20d3fcf11e95ecaa039c1bf85e6ab38098047b
Nguyen The Khiem <126439080+thekhiem14@users.noreply.github.com>
2024-07-30 22:23:23 +0700

    Fix quyền bên backend

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -63,15 +63,22 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/product-category/create
 module.exports.createPost = async (req, res) => {
-  if (req.body.position == "") {
-    const count = await ProductCategory.countDocuments();
-    req.body.position = count + 1;
-  } else {
-    req.body.position = parseInt(req.body.position);
-  }
+  const permissions = res.locals.role.permissions
 
-  const record = new ProductCategory(req.body);
-  await record.save();
+  if(permissions.includes("products-category_create")){
+    if (req.body.position == "") {
+      const count = await ProductCategory.countDocuments();
+      req.body.position = count + 1;
+    } else {
+      req.body.position = parseInt(req.body.position);
+    }
+  
+    const record = new ProductCategory(req.body);
+    await record.save();
+  }
+  else{
+    return
+  }
 
   res.redirect(`${systemConfig.prefixAdmin}/product-category`);
 }
