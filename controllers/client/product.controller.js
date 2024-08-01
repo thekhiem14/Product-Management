3928d23c642dac651c03efb357935da5c63092db
Nguyen The Khiem <126439080+thekhiem14@users.noreply.github.com>
2024-08-01 12:17:18 +0700

    TRang danh sách sản phẩm clinet

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,7 +1,7 @@
 const Product = require("../../models/product.model")
-// const ProductCategory = require("../../models/product-category.model");
+const ProductCategory = require("../../models/product-category.model");
 const productsHelper = require("../../helper/product");
-// const productsCategoryHelper = require("../../helpers/products-category");
+const productsCategoryHelper = require("../../helper/products-category");
 // [GET] /product
 module.exports.index = async (req,res) => {
      const products = await Product.find({
@@ -17,6 +17,7 @@ module.exports.index = async (req,res) => {
      });
      // gọi file pug
 }
+
 // [GET] /product/:slug
 module.exports.detail = async (req,res) => {
      const find = {
@@ -32,3 +33,35 @@ module.exports.detail = async (req,res) => {
           product: product
      })
 }
+
+// [GET] /product/:slugCategory
+module.exports.category = async (req,res) => {
+     try {
+          const category = await ProductCategory.findOne({
+               slug: req.params.slugCategory,
+               status: "active",
+               deleted: false,
+          })
+          
+          const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+
+          const listSubCategoryId = listSubCategory.map(item => item.id);
+  
+
+          const products = await Product.find({
+               product_category_id: {$in: [category.id, ...listSubCategoryId]},
+               deleted: false
+          }).sort({position: "desc"})
+
+          const newProducts = productsHelper.priceNewProducts(products)
+          
+          res.render("client/pages/product/index", {
+               pageTitle: category.title,
+               products: newProducts,
+          });
+     } catch (error) {
+          console.error('Lỗi khi tìm kiếm danh mục');
+          res.status(404).send("Không tìm thấy danh mục");
+     }
+}
+
