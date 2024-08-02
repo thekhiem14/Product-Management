2e5c044c497e68fff6bf71ab6c9cad6f423f58c5
Nguyen The Khiem <126439080+thekhiem14@users.noreply.github.com>
2024-08-02 15:34:26 +0700

    Trang giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,35 @@
 const Cart = require("../../models/cart.model")
+const Product = require("../../models/product.model")
+
+const productHelper = require("../../helper/product") 
+// [GET] /cart
+module.exports.index = async (req,res) => {
+  const cartId = req.cookies.cartId
+
+  const cart = await Cart.findOne({
+    _id: cartId
+  })
+
+  if(cart.products.length > 0){
+    for(const item of cart.products){
+      const productId = item.product_id
+
+      const productInfo = await Product.findOne({
+        _id: productId
+      }).select("title thumbnail slug price discountPercentage")
+
+      item.productInfo = productInfo
+      item.productInfo.priceNew = productHelper.priceNewProduct(item.productInfo)
+    }
+  } 
+
+  cart.totalPrice = cart.products.reduce((sum, item) => sum + item.quantity * item.productInfo.priceNew, 0)
+
+  res.render("client/pages/cart/index", {
+    pageTitle: "Trang chủ",
+    cartDetail: cart
+}) 
+}
 
 // POST /cart/add/:productId
 module.exports.add = async (req,res) => {
